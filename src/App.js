b0f6c86c55d3429f3d7e3b18af1ecfa4b19acf68
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-03-27 23:46:57 -0300

    Variaveis e interpolacoes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const name = 'Miguel'
+
+  function sum(a,b){
+    return a + b
+  }
+const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá React!</h1>
+      <p>Meu primeiro App</p>
+      <p>Olá, {name}</p>
+      <p>Soma {sum(4,5)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
