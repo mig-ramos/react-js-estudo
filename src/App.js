08ad571f8fc82eba5d9c4bc922291c3180be80e5
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-03-28 18:21:43 -0300

    Uso de variaveis como parametro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 
 function App() {
 
-  const nome = 'Arcanjo'
+
 
   return (
     <div className="App">
-        <HelloWorld />
-        <SayMyName nome = {nome} />
+ 
+        <Pessoa  
+        nome="Miguel"
+        idade ="62" 
+        profissao = "Desenvolvedor" 
+        foto="https://via.placeholder.com/150" 
+        />
     </div>
   );
 }
