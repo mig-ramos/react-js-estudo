971e23269ce5f2e4edcb47dfdaf1a61f5bfadc26
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-03-28 20:00:26 -0300

    Adicao de css no componente

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase';
 
 
 function App() {
@@ -10,13 +11,16 @@ function App() {
 
   return (
     <div className="App">
- 
+        <h1>Testando css</h1>
+        <Frase />
+        <Frase />
         <Pessoa  
         nome="Miguel"
         idade ="62" 
         profissao = "Desenvolvedor" 
         foto="https://via.placeholder.com/150" 
         />
+       
     </div>
   );
 }
